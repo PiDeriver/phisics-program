29bf2018143e9359ba17669460fb1f4acd358ce3
PiDeriver <kevinsikes@cableone.net>
2016-02-28 17:47:59 -0700

    Worked on auto installer and it now works

--- Auto-Installer/Move.java
+++ Auto-Installer/Move.java
@@ -1,24 +1,59 @@
-//https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html#move-java.nio.file.Path-java.nio.file.Path-java.nio.file.CopyOption...-
+import java.util.Scanner;
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
 /**
- * Write a description of class Move here.
+ * This will automatically move the app to someone's calculator when run
  * 
- * @author (your name) 
+ * @author Kevin Sikes
  * @version (a version number or a date)
  */
 public class Move
 {
-    
+    String usernm = "";
     /**
-     * Constructor for objects of class Move
+     * receives input from user to find the location of the documents folder
      */
-    public Move()
-    {
+    public void Move(){
+        String input = "";
+        System.out.println("Make sure that the Physics program file is in your documents folder. Then please input your username so it can find the folder. Just type it below and then hit enter.");
+        Scanner inp = new Scanner(System.in);
+        usernm = inp.nextLine();
         move();
     }
-
-    public static Path move(Path source, Path target, CopyOption... options){
-        Path source = ;
-        Path newdir = ;
-        Files.move(source, newdir.resolve(source.getFileName()), ATOMIC_MOVE);
+    
+    /**
+     * physics file MUST be in Documents folder to work
+     * moves the file to the calculator
+     * currently: copies the file from documents to desctop
+     * TODO: have it copy the file to the calculator, need to find out how to acess calculator and where to put file
+     */
+    private void move(){
+        try{
+            Path start = Paths.get("C:/Users/" + usernm +"/Documents/PhysicsCOPYtest.txt");
+            Path newdir = Paths.get("C:/Users/"+ usernm +"/Desktop/PhysicsCOPYtest.txt");//TODO need to find where the calculator is
+            Files.move(start, newdir, StandardCopyOption.ATOMIC_MOVE);
+            System.out.println("The file has been sucsessfully moved to your calculator!");
+        }
+        catch(IOException e){
+            System.out.println("IOException");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch(FileSystemNotFoundException e){
+           System.out.println("File system not found");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch(InvalidPathException e){
+            System.out.println("Invalid path");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
